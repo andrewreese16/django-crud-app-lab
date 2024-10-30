8ec050a369cc044315186c0cd60da18cb59a15f7
andrewreese16 <andrewreese1616@gmail.com>
2024-10-30 15:11:21 -0400

    finished lab

--- my_app/models.py
+++ my_app/models.py
@@ -4,6 +4,17 @@ from django.urls import reverse
 
 # Create your models here.
 
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('toy-detail', kwargs={'pk': self.id})
+
+
 class Dog(models.Model):
   name = models.CharField(max_length=100)
   breed = models.CharField(max_length=100)
--- my_app/views.py
+++ my_app/views.py
@@ -1,10 +1,47 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Dog
+from .models import Dog, Toy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 from .forms import FeedingForm
 
 # Create your views here.
+
+
+def associate_toy(request, dog_id, toy_id):
+    # Adds the row with dog_id and toy_id to the join table in psql
+    Dog.objects.get(id=dog_id).toys.add(toy_id)
+    return redirect("dog-detail", dog_id=dog_id)
+
+
+def remove_toy(request, dog_id, toy_id):
+    dog = Dog.objects.get(id=dog_id)
+    toy = Toy.objects.get(id=toy_id)
+    dog.toys.remove(toy)
+    return redirect("dog-detail", dog_id=dog.id)
+
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = "__all__"
+
+
+class ToyList(ListView):
+    model = Toy
+
+
+class ToyDetail(DetailView):
+    model = Toy
+
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ["name", "color"]
+
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = "/toys/"
 
 
 def home(request):
@@ -22,9 +59,18 @@ def dog_index(request):
 
 def dog_detail(request, dog_id):
     dog_from_db = Dog.objects.get(id=dog_id)
+    toys_dog_doesnt_have = Toy.objects.exclude(
+        id__in=dog_from_db.toys.all().values_list("id")
+    )
     feeding_form = FeedingForm()
     return render(
-        request, "dogs/detail.html", {"dog": dog_from_db, "feeding_form": feeding_form}
+        request,
+        "dogs/detail.html",
+        {
+            "dog": dog_from_db,
+            "feeding_form": feeding_form,
+            "toys": toys_dog_doesnt_have,
+        },
     )
 
 
@@ -33,7 +79,7 @@ def dog_detail(request, dog_id):
 # templates/my_app/dog_form.html
 class DogCreate(CreateView):
     model = Dog
-    fields = "__all__"
+    fields = ["name", "breed", "description", "age"]
     # Go to the models.py file for the Dog model
     # to see where the DogCreate redirects to, after
     # a POST request
@@ -55,4 +101,4 @@ def add_feeding(request, dog_id):
         new_feeding = form.save(commit=False)
         new_feeding.dog_id = dog_id
         new_feeding.save()
-    return redirect('dog-detail', dog_id=dog_id)
+    return redirect("dog-detail", dog_id=dog_id)
